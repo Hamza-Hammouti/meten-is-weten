1726337aee4344a60031c0842eac975c781b563e
HamzaHammouti <hamzahhammouti@gmail.com>
2021-09-15 09:55:38 +0200

    input en if-statement

---
+++ meten-is-weten.py
@@ -0,0 +1,10 @@
+a = input("Welke waarde heeft A?: ")
+b = input("Welke waarde heeft B?: ")
+max = 0
+
+if a > b:
+    max = a
+    print(f"A is het grootste getal. Waarde: {max}")
+else:
+    max = b
+    print(f"B is het grootste getal. Waarde: {max}")
